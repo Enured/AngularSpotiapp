44ac041a18a10f76fd2e66040b7256e4d9b940a7
Enured <cristofer.segundo@gmail.com>
2018-12-26 22:52:38 -0800

    Prepared the routes and modules. Practiced with restcountries API before trying with spotify.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+//importar router y se coloca abajo de browser module en los imports
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { SearchComponent } from './components/search/search.component';
+import { ArtistaComponent } from './components/artista/artista.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+
+// Importar rutas desde app.routes.ts
+import { ROUTES } from './app.routes';
+// importar para hacer peticiones http
+import { HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SearchComponent,
+    ArtistaComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    //La constante del archivo app.routes.ts
+    RouterModule.forRoot( ROUTES, {useHash: true } )
   ],
   providers: [],
   bootstrap: [AppComponent]
